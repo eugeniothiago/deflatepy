187397c725a6d8a6f11e262ba2a4cb76e432f6de
eugeniothiago <thiagoace1@hotmail.com>
2022-06-21 23:30:50 -0300

    finalized script, linted code with black

--- api_call.py
+++ api_call.py
@@ -3,22 +3,22 @@ import pandas as pd
 import sys
 
 
-def api_call(series:str) -> pd.DataFrame:
-    '''
+def api_call(series: str) -> pd.DataFrame:
+    """
     A function that return historical values for Brazil's IPCA (Broad Consumers Price Index) in a Pandas.DataFrame format.
-    
+
     Params:
         series -> specifies the type of values wanted.
             if monthly values are desided, then series='monthly'
-            otherwise series='yearly' 
-    '''
-    base_url =  "http://www.ipeadata.gov.br/api/odata4/Metadados"
-    series_dict = {"yearly":"('PRECOS_IPCAG')", "monthly":"('PRECOS12_IPCA15G12')"}
-    series_type=""
-    if series== "yearly":
-        series_type = series_dict['yearly']
-    elif series=='monthly':
-        series_type = series_dict['monthly']
+            otherwise series='yearly'
+    """
+    base_url = "http://www.ipeadata.gov.br/api/odata4/Metadados"
+    series_dict = {"yearly": "('PRECOS_IPCAG')", "monthly": "('PRECOS12_IPCA15G12')"}
+    series_type = ""
+    if series == "yearly":
+        series_type = series_dict["yearly"]
+    elif series == "monthly":
+        series_type = series_dict["monthly"]
     else:
         print("Incorrect value. Series arguments must be either 'yearly' or 'monthly'.")
         sys.exit(1)
@@ -26,15 +26,21 @@ def api_call(series:str) -> pd.DataFrame:
     data = requests.get(complete_url)
     if data.status_code == requests.codes.ok:
         data = data.json()
-        if 'value' in data:
+        if "value" in data:
             try:
-                data = pd.DataFrame(data['value'])
-                data = data[['SERCODIGO','VALDATA','VALVALOR']]
-                data = data.rename({'SERCODIGO':'series','VALDATA':'date','VALVALOR':'ipca'}, axis=1)
-                data['date'] = pd.to_datetime(data['date'].apply(pd.to_datetime),utc=True,format='%Y-%m-%d')
+                data = pd.DataFrame(data["value"])
+                data = data[["SERCODIGO", "VALDATA", "VALVALOR"]]
+                data = data.rename(
+                    {"SERCODIGO": "series", "VALDATA": "date", "VALVALOR": "ipca"},
+                    axis=1,
+                )
+                data["date"] = pd.to_datetime(
+                    data["date"].apply(pd.to_datetime), utc=True, format="%Y-%m-%d"
+                )
                 return data
             except Exception:
                 return None
 
-if __name__ =='__main__':
+
+if __name__ == "__main__":
     api_call()
