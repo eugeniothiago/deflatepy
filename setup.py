34b60ca918dc31d7c6225b35ec01876ada3e5474
eugeniothiago <thiagoace1@hotmail.com>
2022-06-22 19:32:55 -0300

    added arguments to setuptools, added pypi relevant data

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 import setuptools
 
+long_description = open('README.md').read()
+
+project_links = {
+    "Github":"https://github.com/eugeniothiago/deflatepy"
+}
+
 setuptools.setup(
     name="deflatepybr",
-    version="0.0.1",
+    version="0.0.4",
     author="Thiago Eugênio",
     description="A simple package to deflate BR currency values using IPCA's historical yearly and monthly indexes",
-    packages=["deflator"]
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_links = project_links,
+    packages=["deflator"],
+    readme = "README.md",
+    requires=["requests","pandas"],
 )
